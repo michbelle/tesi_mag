0995e07c286d2441712d9ff9b190615a23dcc0ba
michbelle <mi94bell@gmail.com>
2025-09-23 16:49:44 +0200

    launch world

---
+++ src/tesi_code/ign_world/setup_ign.sh
@@ -0,0 +1,1 @@
+export GZ_SIM_RESOURCE_PATH=/root/ff_ws/src/myTesiCode/rmf_code/ign_world/elettra/modelOUT/
---
+++ src/tesi_code/launch/0s_ign_spawn_world_gazebo.launch.py
@@ -0,0 +1,81 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, SetEnvironmentVariable
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, PythonExpression
+
+from launch.conditions import IfCondition
+
+from launch.actions import ExecuteProcess
+
+def generate_launch_description():
+    # Create the launch configuration variables
+    use_sim_time = LaunchConfiguration('use_sim_time')
+    
+    declare_use_sim_time_cmd = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='true',
+        description='Use simulation (Gazebo) clock if true')
+    
+    world = LaunchConfiguration('world')
+    declare_world_cmd = DeclareLaunchArgument(
+        'world',
+        default_value='elettra',
+        description='World dir to use in Gazebo')
+    
+    world_name = LaunchConfiguration('world_name')
+    declare_world_name_cmd = DeclareLaunchArgument(
+        'world_name',
+        default_value='ign-elettra-map.world',
+        description='World file to use in Gazebo')
+    
+    headless = LaunchConfiguration("headless")
+    headless_dcl = DeclareLaunchArgument(
+        'headless',
+        default_value='False')
+    
+    #add general path for simulation 
+    
+    new_path = PathJoinSubstitution([get_package_share_directory("tesi_code"), 'ign_world', world, "modelOUT"])
+    # new_path = os.path.join([get_package_share_directory("tesi_code"), 'ign_world', world, world_name])
+    current_path = os.environ.get('GZ_SIM_RESOURCE_PATH', '')
+    if current_path:
+        new_path = f"{current_path}:{new_path}"
+    set_env_cmd=SetEnvironmentVariable('GZ_SIM_RESOURCE_PATH', new_path)
+    
+    
+    
+    gz_world_arg = PathJoinSubstitution([get_package_share_directory("tesi_code"), 'ign_world', world, world_name])
+    # Include the gz sim launch file  
+    gz_sim_share = get_package_share_directory("ros_gz_sim")
+    gz_sim = ExecuteProcess(
+        cmd=['gz', 'sim', '-r', '-s', gz_world_arg],
+        output='screen',
+    )
+    
+    gz_sim_gui = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(os.path.join(gz_sim_share, "launch", "gz_sim.launch.py")),
+        condition=IfCondition(PythonExpression(["not ", headless])),
+        launch_arguments={
+            "gz_args" : ["-v4 -g "],
+        }.items(),
+    )
+    
+    # Create the launch description and populate
+    ld = LaunchDescription()
+    #set env variable
+    # Declare the launch options
+    ld.add_action(headless_dcl)
+    ld.add_action(declare_use_sim_time_cmd)
+    ld.add_action(declare_world_cmd)
+    ld.add_action(declare_world_name_cmd)
+    ld.add_action(set_env_cmd)
+
+    # Launch Gazebo
+    ld.add_action(gz_sim)
+    ld.add_action(gz_sim_gui)
+
+    return ld
